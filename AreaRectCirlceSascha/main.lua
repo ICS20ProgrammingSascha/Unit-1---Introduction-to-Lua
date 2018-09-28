b1eab21668d0c70771b4d2b0f2b608c06d5dc5e7
saschamotz <sascha.motz@ocsbstudent.ca>
2018-09-27 21:21:37 -0400

    Day13 MovingObjects start

--- AreaRectCirlceSascha/main.lua
+++ AreaRectCirlceSascha/main.lua
@@ -1,21 +1,21 @@
 -- Title: Area Rect Circle
 -- Name: Sascha Motz
 -- Course: ICS20/3C
--- This program displays a recdtangle and shows its area
+-- This program displays a rectangle and shows its area
 
 -- Create my local variables
 local areaText
 local textSize = 50
 local myRectangle
 local widthOfRectangle = 350
-local heightOfRectanngle = 200
+local heightOfRectangle = 200
 local areaOfRectangle
 
 -- set the background colour of my screen. Remeber that colours are between 0 and 1.
 display.setDefault("background", 30/255, 141/255, 243/255)
 
 -- to remove status bar
-display.setStatusBar(display.HiddenStatusbar)
+display.setStatusBar(display.HiddenStatusBar)
 
 --draw the rectangle that is half the width and height of the screen size.
 myRectangle = display.newRect(0, 0, widthOfRectangle, heightOfRectangle)
@@ -23,17 +23,73 @@ myRectangle = display.newRect(0, 0, widthOfRectangle, heightOfRectangle)
 --anchor the rectangle in the top left conor of the screen and set its (x,y) position.
 myRectangle.anchorX = 0
 myRectangle.anchorY = 0
-myRectangle.x = 20
+myRectangle.x = 70
 myRectangle.y = 20
 
 --set the width of the border
-myRectangle.strokeWidth = 20
+myRectangle.strokeWidth = 22
 
 --set the colour of the rectangle
-myRectangle:setFillColor(0.7, 0.1, 0.3)
+myRectangle:setFillColor(0.2, 0.7, 0.3)
 
 -- set the color of the border
-myRectangle:setStrokeColor (1, 0, 0)
+myRectangle:setStrokeColor (0, 3, 0)
 
 -- calculate the area
 areaOfRectangle = widthOfRectangle * heightOfRectangle
+
+-- write the area on the screen. Take into consideration the size of the 
+-- font when positioning it on the screen
+areaText = display.newText("The area of the rectangle with a width of \n" ..
+	                       widthOfRectangle .. " and a height of " .. heightOfRectangle ..  " is " .. 
+	                       areaOfRectangle .. " pixels^2.", 0, 0, Arial, textSize)
+-- anchor the text and its (x,y) position
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 20
+areaText.y = display.contentHeight/2
+
+-- set the colour of the newText
+areaText:setTextColor(0, 3, 0)
+
+----------------------------------------------------------------------------------
+local myCircle
+local xCenter = 600
+local yCenter = 450
+local radius = 100
+local pi = 3.14
+
+--draw the circle 
+myCircle = display.newCircle( xCenter, yCenter, radius)
+
+--anchor the circle in the top left conor of the screen and set its (x,y) position.
+myCircle.anchorX = 0
+myCircle.anchorY = 0
+myCircle.x = 650
+myCircle.y = 20
+
+--set the width of the border
+myCircle.strokeWidth = 8
+
+--set the colour of the rectangle
+myCircle:setFillColor(204/255, 153/255, 255/255)
+
+-- set the color of the border
+myCircle:setStrokeColor (102/255, 0, 204/255)
+
+-- calculate the area
+areaOfCircle = radius*radius * pi
+
+-- write the area on the screen. Take into consideration the size of the 
+-- font when positioning it on the screen
+areaText = display.newText("The area of the circle with a radius of \n" ..
+	                       radius ..  " is " .. 
+	                       areaOfCircle .. " pixels^2.", 0, 0, Arial, textSize)
+-- anchor the text and its (x,y) position
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 20
+areaText.y = (display.contentHeight/2) + 150
+
+-- set the colour of the newText
+areaText:setTextColor(102/255, 0, 204/255)
--- HelloWorldSascha/main.lua
+++ HelloWorldSascha/main.lua
@@ -3,21 +3,28 @@
 -- This program displays hello, World on the iPad simulator and "hellooooooo!" to the comand terminal
 -----------------------------------------------------------------------------------------
 
--- print "Hello, World" to the command ternminal
-print ("***Hellooooooo!")
+-- print "Hello, World!" to the command ternminal
+print ("***Hello World!")
+
+-- print "This is awesome" to the command ternminal
+print ("***This is awesome!")
 
 -- hide the staus bar
 display.setStatusBar(display.HiddenStatusBar)
 
 -- sets the background colour
-display.setDefault("background", 204/255, 204/255, 255/255 )
+display.setDefault("background", 255/255, 153/255, 204/255 )
 
 --create a local variable
 local textObject
 
 -- display text on the screen a position x = 500 and y = 500 with
+-- a default font style and font size of 100
+textObject = display.newText( "Hello, Ms.Raffin!", 500, 500, nil, 100 )
+
+-- display text on the screen a position x = 500 and y = 500 with
 -- a default font style and font size of 50
-textObject = display.newText( "Hello, Sascha!", 500, 500, nil, 50 )
+textObject = display.newText( "By: Sascha Motz", 500, 600, nil, 50 )
 
 -- sets the colour of the text
 textObject:setTextColor(0, 0, 0)
