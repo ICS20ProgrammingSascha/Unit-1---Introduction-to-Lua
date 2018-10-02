3a6c13d1ebffa534d581732f9d2c12b5c2bdf50f
saschamotz <sascha.motz@ocsbstudent,ca>
2018-10-02 14:11:56 -0400

    made the star move to the left

--- MovingImagesSaschaM/main.lua
+++ MovingImagesSaschaM/main.lua
@@ -7,10 +7,11 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 --global variables
-scrollSpeed = 3
+scrollSpeedBeetleship= 3
 
 -- background image with width and height
 local beetleship = display.newImageRect("Images/background.png", 2048, 1536)
+
 
 -- character image with width and height
 local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
@@ -31,7 +32,7 @@ transition.fadeIn( beetleship, { time=500 } )
 --Description: This function adds the scroll speed to the x-value of the ship
 local function MoveShip (event)
 	--add the scroll speed to the x-value of the ship
-	beetleship.x = beetleship.x + scrollSpeed
+	beetleship.x = beetleship.x + scrollSpeedBeetleship
 	-- change the transparency of the ship every time it moves so that it fades out
 	beetleship.alpha = beetleship.alpha + 0.01
 end
@@ -40,12 +41,13 @@ end
 Runtime:addEventListener("enterFrame", MoveShip)
 
 --------------------------------------------------------------------------------------------
+scrollSpeedStar = 5
 
 -- character image with width and height
 local star = display.newImageRect("Images/star.png", 500, 500)
 
 -- set the image to be transparent
-star.alpha = 0
+star.alpha = 0.5
 
 --set the initial x and y position of star
 star.x = 400
@@ -60,7 +62,7 @@ transition.fadeOut( star, { time=2000 } )
 --Description: This function adds the scroll speed to the x-value of the star
 local function MoveStar (event)
 	--add the scroll speed to the x-value of the star
-	star.x = star.x + scrollSpeed + 3
+	star.x = star.x - scrollSpeedStar + 3
 	-- change the transparency of the star every time it moves so that it fades out
 	star.alpha = star.alpha + 0.01
 end
