7ddf429e9aa69abde6eaaa18ec9c65704312459d
saschamotz <sascha.motz@ocsbstudent.ca>
2018-10-17 20:53:09 -0400

    finished

--- NumericTextFieldsSaschaM/main.lua
+++ NumericTextFieldsSaschaM/main.lua
@@ -15,9 +15,7 @@ display.setDefault("background", 255/255, 153/255, 255/255)
 --------------------------------------------------------------------------------------------
 
 -- create local variables
-local questionObject1
-local questionObject2
-local questionObject3
+local questionObject
 local correctObject
 local incorrectObject
 local numericField
@@ -31,37 +29,43 @@ local correctAnswer
 --------------------------------------------------------------------------------------------
 
 local function AskQuestion()
+
 	-- generate 2 random numbers between a max. and a min. number
 	randomNumber1 = math.random(10,20)
 	randomNumber2 = math.random(10,20)
 	randomOperator = math.random(1,3)
 
-	if ( randomOperator == "1") then
+	if ( randomOperator == 1) then
 
-	correctAnswer = randomNumber1 + randomNumber2
+		correctAnswer = randomNumber1 + randomNumber2
 
-	-- create question in text object
-	questionObject1.text = randomNumber1.. " + " ..  randomNumber2 .. " = "
+		-- create question in text object
+		questionObject.text = randomNumber1.. " + " ..  randomNumber2 .. " = "
 
-	elseif ( randomOperator == "2") then
+	elseif ( randomOperator == 2) then
 
-	correctAnswer = randomNumber1 - randomNumber2
+		correctAnswer = randomNumber1 - randomNumber2
 
-	-- create question in text object
-	questionObject2.text = randomNumber1.. " - " ..  randomNumber2 .. " = "
+		-- create question in text object
+		questionObject.text = randomNumber1.. " - " ..  randomNumber2 .. " = "
 
-	elseif ( randomOperator == "3") then
+	elseif ( randomOperator == 3) then
 
-	correctAnswer = randomNumber1 * randomNumber2
+		correctAnswer = randomNumber1 * randomNumber2
 
-	-- create question in text object
-	questionObject3.text = randomNumber1.. " * " ..  randomNumber2 .. " = "
+		-- create question in text object
+		questionObject.text = randomNumber1.. " * " ..  randomNumber2 .. " = "
 	end
-
+	
 end
 
 local function HideCorrect()
 	correctObject.isVisible = false
+	AskQuestion()
+end
+
+local function HideIncorrect()
+	incorrectObject.isVisible = false
 	AskQuestion()
 end
 
@@ -86,8 +90,10 @@ local function NumericFieldListener( event )
 				incorrectObject.isVisible = true
 				timer.performWithDelay(2000, HideIncorrect)
 
-
 			end
+
+			--clear text field
+			event.target.text = ""
 		end
 	end
 
@@ -96,8 +102,8 @@ local function NumericFieldListener( event )
 	--------------------------------------------------------------------------------------------
 
 	-- display a question and sets the colour
-	questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
-	questionObject:setTextColor(0,255, 0,255, 0,255)
+	questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 55 )
+	questionObject:setTextColor(0,255, 0,255, 255,255)
 
 	-- create the correct text object and make it invisible
 	correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
@@ -105,12 +111,12 @@ local function NumericFieldListener( event )
 	correctObject.isVisible = false
 
 		-- create the incorrect text object and make it invisible
-	incorrectObject = display.newText( "Incorrect! Nice Try!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+	incorrectObject = display.newText( "Incorrect! Nice Try!", display.contentWidth/2, display.contentHeight*2/3, nil, 110)
 	incorrectObject:setTextColor(255,255, 0,255, 0,255)
 	incorrectObject.isVisible = false
 
 	-- Create the numeric field
-	numericField = native.newTextField( display.contentWidth/5, display.contentHeight/2, 150, 80 )
+	numericField = native.newTextField( display.contentWidth/1.7, display.contentHeight/2, 200, 110 )
 	numericField.inputType = "number"
 
 	-- add the event listener for the numeric field
